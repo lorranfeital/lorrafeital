fa48a68cd210f6233e972087a4600952eab313d7
lorranfeital <143146649+lorranfeital@users.noreply.github.com>
2023-08-25 11:51:18 -0300

    Create getRedditPosts.js

---
+++ netlify/functions/getRedditPosts.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+exports.handler = async function(event, context) {
+    const accessToken = event.headers.authorization;
+    const subreddit = event.queryStringParameters.subreddit || 'all';
+
+    const config = {
+        headers: {
+            'Authorization': 'bearer ' + accessToken,
+            'User-Agent': 'Sonda Social'
+        }
+    };
+
+    try {
+        const response = await axios.get(`https://oauth.reddit.com/r/${subreddit}/hot`, config);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response.data.data.children)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message })
+        };
+    }
+}
