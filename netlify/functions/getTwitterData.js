2c49dc51fe8ffc36ac71786b2df927ba4acdb457
lorranfeital <143146649+lorranfeital@users.noreply.github.com>
2023-08-25 09:43:34 -0300

    Create getTwitterData.js

---
+++ netlify/functions/getTwitterData.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+exports.handler = async function(event, context) {
+    const API_URL = "https://api.twitter.com/1.1/trends/place.json?id=1"; // Exemplo: pegar trending topics mundiais
+
+    const HEADERS = {
+        Authorization: 'AAAAAAAAAAAAAAAAAAAAAL5hpgEAAAAAig%2F9o68S1RezBi89n%2BjV7v2uHJQ%3DxxyykTOYvSNwHogNuW1Mi06ZkrihBXIvGXAVimz8I8Wfi2gped', // Substitua SEU_BEARER_TOKEN pelo seu Bearer Token
+        "Content-Type": "application/json"
+    };
+
+    try {
+        const response = await axios.get(API_URL, { headers: HEADERS });
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response.data)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Failed fetching data" })
+        };
+    }
+};
